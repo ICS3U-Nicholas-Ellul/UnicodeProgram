408968a7374f112b80496a568cd8715ca632f6e6
pirates19 <nicholas.ellul@ocsbstudent.ca>
2015-10-16 14:39:03 -0400

    Finished Program uses for loop to calculate unicode.

---
+++ UnicodeProgram/UnicodeProject.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace UnicodeProgram
+{
+    partial class UnicodeProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstListBox = new System.Windows.Forms.ListBox();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstListBox
+            // 
+            this.lstListBox.FormattingEnabled = true;
+            this.lstListBox.Location = new System.Drawing.Point(85, 124);
+            this.lstListBox.Name = "lstListBox";
+            this.lstListBox.Size = new System.Drawing.Size(120, 95);
+            this.lstListBox.TabIndex = 0;
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(44, 33);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(209, 48);
+            this.lblInstructions.TabIndex = 2;
+            this.lblInstructions.Text = "Characters and \r\nTheir Unicode values";
+            this.lblInstructions.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(85, 95);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(120, 23);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Calculate";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // UnicodeProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.lstListBox);
+            this.Name = "UnicodeProject";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstListBox;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnGo;
+    }
+}
+
---
+++ UnicodeProgram/UnicodeProject.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Created by: Nicholas  Ellul
+ * Created on: 16-Oct-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 3-11
+ * This program lists the characters and unicodes from  A-Z.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnicodeProgram
+{
+    public partial class UnicodeProject : Form
+    {
+        public UnicodeProject()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            //input
+            string unicodeAndChar;
+          
+            //process
+            this.lstListBox.Items.Clear();
+            for(int loopCounter = 65; loopCounter<91; loopCounter++)
+            {
+        
+               unicodeAndChar = Char.ConvertFromUtf32(loopCounter) + " --> " + loopCounter;
+
+                //output
+                this.lstListBox.Items.Add(unicodeAndChar);
+            }
+        }
+    }
+}
